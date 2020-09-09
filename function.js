24c099745beafd9393794cc58a58e74b2c031969
AlQasmSystem <alqasmsystem@localhost.localdomain>
2020-09-09 22:36:54 +0000

    function persegi p

---
+++ function.js
@@ -0,0 +1,17 @@
+var panjang = parseInt(prompt('Masukkan Nilai Panjang :'));
+var lebar = parseInt(prompt('Masukkan Nilai Lebar :'));
+
+
+function jumlahLuas(p , l){
+    // var volumeA;
+    // var total;
+
+    // volumeA = p * l;
+
+    // total = volumeA;
+
+    // return total;
+    return p * l;
+}
+
+alert("luas " + jumlahLuas(panjang,lebar));
